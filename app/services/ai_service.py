ddf41ef099e260fb869f33cf42068a5a3323566c
Abhishekp1428-wq <abhishekp1428@gmail.com>
2026-08-09 19:34:37 +0530

    Add AI image analysis service

---
+++ app/services/ai_service.py
@@ -0,0 +1,53 @@
+import os
+import base64
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def analyze_image(image_path):
+
+    image_base64 = encode_image(image_path)
+
+    response = client.chat.completions.create(
+      model="qwen/qwen3.6-27b",
+      messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": """
+Analyze this image.
+
+Return only JSON.
+
+{
+    "problem":"",
+    "severity":"",
+    "department":"",
+    "description":""
+}
+"""
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{image_base64}"
+                    }
+                }
+            ]
+        }
+    ]
+)
+    return response.choices[0].message.content
